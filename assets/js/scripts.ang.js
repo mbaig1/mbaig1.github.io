d44119a1cc59c52aa683e858bf0c23a3e379af02
harrisjm <hjmotiwala@utexas.edu>
2018-10-16 04:36:28 -0500

    finalize template

--- assets/js/scripts.ang.js
+++ assets/js/scripts.ang.js
@@ -6,27 +6,6 @@ var app = angular.module('myPage', ['ngTouch']);
 app.controller('myPageCtrl', function($scope,$http){
 
 	/* ------------------------------ */
-	/* GOOGLE MAP
-	/* ------------------------------ */
-
-	$scope.mapInitialization = function() {
-		$scope.myLatlng = new google.maps.LatLng(33.96290,-118.43589),
-		$scope.mapOptions = {
-			zoom: 14,
-			scrollwheel: false,
-			center: $scope.myLatlng
-		},
-		$scope.map = new google.maps.Map(document.getElementById('map'), $scope.mapOptions),
-		$scope.marker = new google.maps.Marker({
-			position: $scope.myLatlng,
-			map: $scope.map,
-			icon: "images/map.png"
-		});
-	}
-
-	$scope.mapInitialization();
-
-	/* ------------------------------ */
 	/* SCROLL BUTTON
 	/* ------------------------------ */
 
@@ -119,7 +98,7 @@ app.controller('myPageCtrl', function($scope,$http){
 	/* ------------------------------ */
 	/* ANIMATED COUNTER
 	/* ------------------------------ */
-	
+
 	$scope.counterInit = function() {
 		$('.statistic').on('inview.uk.scrollspy', function(){
 			$scope.numAnim1 = new CountUp('statistic_counter_1', 0, 100, 0, 3.5);
@@ -132,7 +111,7 @@ app.controller('myPageCtrl', function($scope,$http){
 			$scope.numAnim4.start();
 		});
 	}
-	
+
 	$scope.counterInit();
 
 	/* ------------------------------ */
@@ -250,260 +229,12 @@ app.controller('myPageCtrl', function($scope,$http){
 					engine.rain([ [3, 3, 0.88], [5, 5, 0.9], [6, 2, 1] ], 100);
 				};
 				$scope.image.crossOrigin = 'anonymous';
-				$scope.image.src = 'images/photo_1.jpg';
+				$scope.image.src = 'https://res.cloudinary.com/dzixj0ktk/image/upload/v1539666857/photo_1_bhbsut.jpg';
 			}
 			$scope.run();
 		}
-		$scope.particlesBlockInsert = function() {
-			$scope.container.append('<div id="particles_block"></div>');
-			particlesJS('particles_block',
-			  {
-				"particles": {
-				  "number": {
-					"value": 80,
-					"density": {
-					  "enable": true,
-					  "value_area": 800
-					}
-				  },
-				  "color": {
-					"value": "#ffffff"
-				  },
-				  "shape": {
-					"type": "circle",
-					"stroke": {
-					  "width": 0,
-					  "color": "#000000"
-					},
-					"polygon": {
-					  "nb_sides": 5
-					},
-					"image": {
-					  "src": "img/github.svg",
-					  "width": 100,
-					  "height": 100
-					}
-				  },
-				  "opacity": {
-					"value": 0.5,
-					"random": false,
-					"anim": {
-					  "enable": false,
-					  "speed": 1,
-					  "opacity_min": 0.1,
-					  "sync": false
-					}
-				  },
-				  "size": {
-					"value": 5,
-					"random": true,
-					"anim": {
-					  "enable": false,
-					  "speed": 40,
-					  "size_min": 0.1,
-					  "sync": false
-					}
-				  },
-				  "line_linked": {
-					"enable": true,
-					"distance": 150,
-					"color": "#ffffff",
-					"opacity": 0.4,
-					"width": 1
-				  },
-				  "move": {
-					"enable": true,
-					"speed": 6,
-					"direction": "none",
-					"random": false,
-					"straight": false,
-					"out_mode": "out",
-					"attract": {
-					  "enable": false,
-					  "rotateX": 600,
-					  "rotateY": 1200
-					}
-				  }
-				},
-				"interactivity": {
-				  "detect_on": "canvas",
-				  "events": {
-					"onhover": {
-					  "enable": true,
-					  "mode": "repulse"
-					},
-					"onclick": {
-					  "enable": true,
-					  "mode": "push"
-					},
-					"resize": true
-				  },
-				  "modes": {
-					"grab": {
-					  "distance": 400,
-					  "line_linked": {
-						"opacity": 1
-					  }
-					},
-					"bubble": {
-					  "distance": 400,
-					  "size": 40,
-					  "duration": 2,
-					  "opacity": 8,
-					  "speed": 3
-					},
-					"repulse": {
-					  "distance": 200
-					},
-					"push": {
-					  "particles_nb": 4
-					},
-					"remove": {
-					  "particles_nb": 2
-					}
-				  }
-				},
-				"retina_detect": true,
-				"config_demo": {
-				  "hide_card": false,
-				  "background_color": "#555555",
-				  "background_image": "",
-				  "background_position": "50% 50%",
-				  "background_repeat": "no-repeat",
-				  "background_size": "cover"
-				}
-			  }
-
-			)
-		}
-		$scope.winterBlockInsert = function() {
-			$scope.container.append('<div id="winter_block"></div>');
-			particlesJS('winter_block',
-				{
-					"particles": {
-						"number": {
-							"value": 400,
-							"density": {
-								"enable": true,
-								"value_area": 800
-							}
-						},
-						"color": {
-							"value": "#fff"
-						},
-						"shape": {
-							"type": "circle",
-							"stroke": {
-								"width": 0,
-								"color": "#000000"
-							},
-							"polygon": {
-								"nb_sides": 5
-							},
-							"image": {
-								"src": "img/github.svg",
-								"width": 100,
-								"height": 100
-							}
-						},
-						"opacity": {
-							"value": 0.5,
-							"random": true,
-							"anim": {
-								"enable": false,
-								"speed": 1,
-								"opacity_min": 0.1,
-								"sync": false
-							}
-						},
-						"size": {
-							"value": 7,
-							"random": true,
-							"anim": {
-								"enable": false,
-								"speed": 40,
-								"size_min": 0.1,
-								"sync": false
-							}
-						},
-						"line_linked": {
-							"enable": false,
-							"distance": 500,
-							"color": "#ffffff",
-							"opacity": 0.4,
-							"width": 2
-						},
-						"move": {
-							"enable": true,
-							"speed": 6,
-							"direction": "bottom",
-							"random": false,
-							"straight": false,
-							"out_mode": "out",
-							"bounce": false,
-							"attract": {
-								"enable": false,
-								"rotateX": 600,
-								"rotateY": 1200
-							}
-						}
-					},
-					"interactivity": {
-						"detect_on": "canvas",
-						"events": {
-							"onhover": {
-								"enable": true,
-								"mode": "bubble"
-							},
-							"onclick": {
-								"enable": true,
-								"mode": "repulse"
-							},
-							"resize": true
-						},
-						"modes": {
-							"grab": {
-								"distance": 400,
-								"line_linked": {
-									"opacity": 0.5
-								}
-							},
-							"bubble": {
-								"distance": 400,
-								"size": 4,
-								"duration": 0.3,
-								"opacity": 1,
-								"speed": 3
-							},
-							"repulse": {
-								"distance": 200,
-								"duration": 0.4
-							},
-							"push": {
-								"particles_nb": 4
-							},
-							"remove": {
-								"particles_nb": 2
-							}
-						}
-					},
-					"retina_detect": true
-				}
-
-			)
-		}
 		if( !(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) ) {
-			if ($scope.html.hasClass('water_effect')) {
-				$scope.container.ripples({
-					resolution: 512,
-					dropRadius: 20,
-					perturbance: 0.04
-				});
-			} else if ($scope.html.hasClass('particles_effect')) {
-				$scope.particlesBlockInsert();
-			} else if ($scope.html.hasClass('winter_effect')) {
-				$scope.winterBlockInsert();
-			} else if ($scope.html.hasClass('rainy_effect')) {
+			if ($scope.html.hasClass('rainy_effect')) {
 				$scope.rainyBlockInsert();
 			}
 		}
@@ -544,7 +275,7 @@ app.controller('myPageCtrl', function($scope,$http){
 								.find('.error_response').html(resp.msg).fadeIn(1000);
 						}
 					},
-					url: "http://google.us3.list-manage.com/subscribe/post?u=54cac12d99d1b2a0c0e0177b4&amp;id=d5469b7ba3" // Replace this with your own mailchimp post URL. Don't remove the "". Just paste the url inside "". 
+					url: "http://google.us3.list-manage.com/subscribe/post?u=54cac12d99d1b2a0c0e0177b4&amp;id=d5469b7ba3" // Replace this with your own mailchimp post URL. Don't remove the "". Just paste the url inside "".
 				});
 			})(i);
 		}
