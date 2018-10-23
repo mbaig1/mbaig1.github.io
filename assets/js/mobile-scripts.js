48866fdc8d273bc205a9c64f5e7565000ecff9d8
harrisjm <hjmotiwala@utexas.edu>
2018-10-23 00:51:34 -0500

    create mobile scripts file with zoomed in background image for smaller screens

---
+++ assets/js/mobile-scripts.js
@@ -0,0 +1,6 @@
+var width = document.body.clientWidth;
+var img = document.getElementById("background-image-rain");
+
+if(width <= 800) {
+  img.src = "https://res.cloudinary.com/dzixj0ktk/image/upload/v1540273251/zoomed-in-skyscraper_g72aug.jpg";
+}
