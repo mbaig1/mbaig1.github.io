879b1989d4283eb45eb33fbadbbde9481e46369e
harrisjm <hjmotiwala@utexas.edu>
2018-10-23 22:25:29 -0500

    fix mobile size image

--- assets/js/mobile-scripts.js
+++ assets/js/mobile-scripts.js
@@ -1,6 +1,19 @@
-var width = document.body.clientWidth;
-var img = document.getElementById("background-image-rain");
+window.addEventListener("resize", changeTopImage);
 
-if(width <= 800) {
-  img.src = "https://res.cloudinary.com/dzixj0ktk/image/upload/v1540273251/zoomed-in-skyscraper_g72aug.jpg";
+function changeTopImage() {
+  var width = document.body.clientWidth;
+  var img = document.getElementById("background-image-rain");
+
+  console.log("changeImage");
+  console.log(width);
+
+  if(width <= 800) {
+    img.src = "https://res.cloudinary.com/dzixj0ktk/image/upload/v1540273251/zoomed-in-skyscraper_g72aug.jpg";
+  } else {
+    img.src = "https://res.cloudinary.com/dzixj0ktk/image/upload/o_65/v1539797933/photo_1_qwxu9e.jpg";
+  }
+
+  run();
 }
+
+changeTopImage();
